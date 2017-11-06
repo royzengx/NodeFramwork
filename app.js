9742693cd26eb6b39f19489355cb814156bd9c05
RoyZeng <roy@192.168.100.105>
2017-11-06 17:34:52 +0800

    add log4j

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const koaRouter = require('koa-router')();
 const json = require('koa-json');
 const logger = require('koa-logger');
 const jwt = require('koa-jwt');
+const log4js = require('koa-log4')
+
+// log4j import.
+require('./util/log4j')
+const log4j = log4js.getLogger('mgtest')
+log4j.info('--------step into koa-------------')
+
 
 const app = new Koa();
 // Custom 401 handling (first middleware)
@@ -35,6 +42,8 @@ app.use(jwt({
 }).unless({
     path: [/^\/auth\/login/]
 }));
+
+app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
 
 const indexRouters = require('./api/index')
 app.use(indexRouters.routes()).use(indexRouters.allowedMethods());
