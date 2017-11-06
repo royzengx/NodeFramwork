94fd9ab2f45696a604ede47f961ff076ede6308e
RoyZeng <roy@192.168.100.105>
2017-11-06 18:04:40 +0800

    add interr*

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const logger = require('koa-logger');
 const jwt = require('koa-jwt');
 const log4js = require('koa-log4')
 
-// log4j import.
+// import log4j setting.
 require('./util/log4j')
 const log4j = log4js.getLogger('mgtest')
 log4j.info('--------step into koa-------------')
 
-
+// Oh hooo~~ init daddy object.ß
 const app = new Koa();
 // Custom 401 handling (first middleware)
 app.use(function (ctx, next) {
+    let url = ctx.url
+    if (url !== '/auth/login') {
+        // check logic here.
+        log4j.info('running:' + url)
+        if (ctx.querystring.indexOf('devmode') > -1) {
+            log4j.debug(ctx)
+        }
+    }
     return next().catch((err) => {
         if (err.status === 401) {
             ctx.status = 401;
@@ -43,7 +51,9 @@ app.use(jwt({
     path: [/^\/auth\/login/]
 }));
 
-app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
+app.use(log4js.koaLogger(log4js.getLogger('http'), {
+    level: 'auto'
+}))
 
 const indexRouters = require('./api/index')
 app.use(indexRouters.routes()).use(indexRouters.allowedMethods());
