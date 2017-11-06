15913d8e7fdfe85db11d073fd9409e8a9b36f21e
Roy Zeng <mail0032@163.com>
2017-11-06 08:39:00 -0600

    wa

--- config/configs.js
+++ config/configs.js
@@ -1,9 +1,9 @@
 var config = {
     dbname: 'test',
-    uname: 'cdb_outerroot',
-    upwd: 'UF5m2DpcodigakdS',
-    host: '591eee9a777d7.sh.cdb.myqcloud.com',
-    port: 12128,
+    uname: 'test',
+    upwd: 'test',
+    host: '127.0.0.1',
+    port: 3306,
     dialect: 'mysql',
     pool: {
         max: 20,
@@ -13,5 +13,3 @@ var config = {
 };
 
 module.exports = config;
-
-//sequelize-auto -o "./model" -d test -h '591eee9a777d7.sh.cdb.myqcloud.com' -u cdb_outerroot -p 12128 -x UF5m2DpcodigakdS -e mysql
